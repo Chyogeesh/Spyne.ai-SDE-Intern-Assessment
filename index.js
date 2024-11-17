10b27a54b835ba01a1cb15dec828f12b9e4bcd09
Ch Yogeesh Sai <108213999+Chyogeesh@users.noreply.github.com>
2024-11-17 20:25:55 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/car-management', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Could not connect to MongoDB:', error));
+
+app.listen(5000, () => {
+    console.log('Server running on http://localhost:5000');
+});
